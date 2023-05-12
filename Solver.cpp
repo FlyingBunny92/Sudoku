1e968e6dee89e8bd5ff072f561cdc95ae36f1ba9
lloucks <flyingbunny92@outlook.com>
2023-05-12 17:14:51 -0600

    working on the sudoki solver

--- Solver.cpp
+++ Solver.cpp
@@ -15,10 +15,25 @@ int addition (int a, int b)
   return r;
 }
 
+int print_board(vector<int> d, int x, int y)
+{
+  cout << "\n int print_board(vector<int> d, int x, int y) \n"  << endl;
+  int count = 0;
+  for(int i = 0; i < y; i++){
+    cout << "\n"  << endl;
+    for(int j = 0; j < x; j++){
+      cout << " " << d[count];
+      count++;
+    }
+  }
 
+  return 0;
+}
 
 int check(vector<int> d, int x, int y, int counter, int num)
 {
+  cout << "int check(vector<int> d, int x, int y, int counter, int num) "  << endl;
+  exit(0);
   int row = counter / x;
   int column = counter % x;
   cout << "counter = " << counter << endl;
@@ -35,6 +50,7 @@ int check(vector<int> d, int x, int y, int counter, int num)
     }
     m = m+x;
   }
+  // check row
   int n = row;
   while(n < d.size()){
     cout << "n = " << n << endl;
@@ -50,24 +66,35 @@ int check(vector<int> d, int x, int y, int counter, int num)
 int solve (vector<int> d, int x, int y, int counter)
 {
   cout << "int solve (std::vector<int> d, int i, int j)";
+  if(counter >= d.size()){
+    return 1;
+  }
+  print_board(d, x, y);
   int size = d.size();
+  /*
   cout << "\n size of the list is : "<<size;
   cout << "\n";
   for(int X : d) 
     cout << "X = " << X << endl;
   cout << "\n d[counter]: = " << d[counter] << endl;
-  counter = 28;
+  */
   if(d[counter]==0)
   {
     for(int i = 1; i < 10; i++){
       int result = check(d, x, y, counter, i);
       if(result == 1){
         d[counter] = i;
-        solve(d, x, y, counter);
+        cout << "d[counter] = i \n";
+        cout << "\n i : " <<  i << endl;
+        exit(0);
+        solve(d, x, y, counter+1);
+        print_board(d, x, y);
       }
     }
+  }else if(counter < d.size()){
+    solve(d, x, y, counter+1);
   }
-  return 0;
+  return 1;
 }
 
 
